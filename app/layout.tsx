daf515f603a507380050fd3df47e4e13dd3a9376
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2025-06-05 22:57:02 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Funds-for-all',
   description: 'This project is a decentralized application (dApp) that allows users to create and participate in fund pools and DAO voting using Ethereum smart contracts.',
+  icons: {
+    icon: "https://avatars.githubusercontent.com/u/213049368?s=200&v=4",
+  },
 }
 
 export default function RootLayout({
